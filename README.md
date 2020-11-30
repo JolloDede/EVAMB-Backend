479b1486b59c13ae9537b0fc80049c76e0539871
JolloDede <jollodede@gmail.com>
2020-11-30 19:51:02 +0100

    create Signup

--- README.md
+++ README.md
@@ -1,43 +1,7 @@
 # EVAMB-Backend
 
-* [X] create server folder
-* [X] npm install express volleyball
-* [X] setup index.js
-* [X] Add get / route
-
 * [X] Messages
   * [X] Add get /messages route
   * [X] Add Post /Messages route
-  
-* [ ] Auth
-  * [ ] Add auth router
-  * [ ] Create user with POST /auth/signup
-    * [ ] validate required fields
-    * [ ] Check if username is unique
-    * [ ] hash password with bcrypt
-    * [ ] insert into db
- * [ ] Login user with POST /auth/login
-    * [ ] validate the user
-    * [ ] check if username in db
-    * [ ] compare password with hashed password in db
-    * [ ] Create and sign a JWT
-    * [ ] Respond with JWT
 
-* [ ] Event
-  * [ ] Add get /events route
-  * [ ] Add Post /events route
-  
-* [ ] Maybe front-end
-  * [ ] Visitors can only see the homepage
-	  * [ ] checkTokenSetUser middleware
-		  * [ ] get token from Authorization header
-			  * [ ] if defined ---
-				  * [ ] Verify the token with the token secret
-				  * [ ] Set req.user to be the decoded verified payload
-			  * [ ] else - move along
-	  * [ ] isLoggedIn middleware
-		  * [ ] if req.user is set - move along
-		  * [ ] else - send an unauthorized error message
-	  * [ ] redirect to login form
-  * [ ] Logged in users can only see their page
 
--- auth/index.js
+++ auth/index.js
@@ -1,12 +1,65 @@
 const express = require('express');
+const Joi = require('joi');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const db = require('../db/connection');
+const users = db.get('users');
+users.createIndex('username', { unique: true });
+
 const router = express.Router();
+
+const schema = Joi.object({
+  username: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
+  password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9]{10,30}$')),
+});
 
 // any route pre-pended with /auth
 router.get('/', (req, res) => {
-    res.json({
-      message:  '🔐'
-    });
+  res.json({
+    message: '🔐'
   });
+});
+
+router.post('/signup', (req, res, next) => {
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    users.findOne({
+      username: req.body.username,
+    }).then(user => {
+      if (user) {
+        // there is already a user in the db with this username...
+        // respond with an error!
+        const error = new Error('That username is not OG. Please choose another one.');
+        res.status(409);
+        next(error);
+      } else {
+        // hash the password
+        bcrypt.hash(req.body.password.trim(), 12).then(hashedPassword => {
+
+          const newUser = {
+            username: req.body.username,
+            password: hashedPassword,
+          };
+          // Add user to db
+          users.insert(newUser).then(insertedUser => {
+            // Respond with Token
+          });
+        });
+      }
+    });
+  }else {
+    res.status(422);
+    next(result.error);
+  }
+});
+
+router.post('/login', (req, res) => {
+
+});
